b41fd730549a17893220461e0c6fc3c73970cd8f
NicolasParizet <72597303+NicolasParizet@users.noreply.github.com>
2021-05-01 12:44:47 +0200

    select req

--- Requêtes.sql
+++ Requêtes.sql
@@ -1,9 +1,22 @@
 /* SELECTION */
 
-SELECT ouvrage.* 
-FROM ouvrage INNER JOIN emprunter ON ouvrage.isbn = emprunter.isbn
-WHERE ouvrage.isbn = 5;
 
+-- infos sur l'ouvrage d'isbn = 5
+SELECT * 
+FROM ouvrage
+WHERE isbn = 5;
+
+
+-- informations sur la classification P
 SELECT *
 FROM classification
-WHERE id_classification = 
+WHERE id_classification LIKE "P";
+
+
+-- liste des ouvrage ayant était validé dans une commande par le personnel nb 2
+SELECT o.*
+FROM ouvrage o INNER JOIN emprunter e ON o.isbn = e.isbn
+INNER JOIN commande c ON e.num = c.num
+WHERE c.id_personne = 2;
+
+
