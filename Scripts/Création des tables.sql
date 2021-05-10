da8f17c03d61d58eaa2cb712635764de74588902
NicolasParizet <72597303+NicolasParizet@users.noreply.github.com>
2021-05-10 17:20:11 +0200

    edit create Table/ jeuData

--- Scripts/Création des tables.sql
+++ Scripts/Création des tables.sql
@@ -7,21 +7,24 @@
 -- Table: Auteur
 ------------------------------------------------------------
 CREATE TABLE Auteur(
+	ID 				   NUMBER NOT NULL ,
 	Nom                VARCHAR2 (32) NOT NULL  ,
 	Prenom             VARCHAR2 (32) NOT NULL  ,
 	Date_de_naissance  DATE  NOT NULL  ,
-	CONSTRAINT Auteur_PK PRIMARY KEY (Nom,Prenom,Date_de_naissance)
+	Nationalite        VARCHAR2 (32) NOT NULL  ,
+	Bio				   CLOB   ,
+	CONSTRAINT Auteur_PK PRIMARY KEY (ID)
 );
 
 ------------------------------------------------------------
 -- Table: Éditeur
 ------------------------------------------------------------
-CREATE TABLE Éditeur(
+CREATE TABLE Editeur(
 	Nom        VARCHAR2 (32) NOT NULL  ,
-	Telephone  NUMBER(10,0)   ,
+	Telephone  VARCHAR2 (32)   ,
 	Email      VARCHAR2 (64)  ,
 	Adresse    VARCHAR2 (256)  ,
-	CONSTRAINT Éditeur_PK PRIMARY KEY (Nom)
+	CONSTRAINT Editeur_PK PRIMARY KEY (Nom)
 );
 
 ------------------------------------------------------------
@@ -32,7 +35,7 @@ CREATE TABLE Adherent(
 	Nom                VARCHAR2 (32) NOT NULL  ,
 	Prenom             VARCHAR2 (32) NOT NULL  ,
 	Date_de_naissance  DATE  NOT NULL  ,
-	Telephone          NUMBER(10,0)  NOT NULL  ,
+	Telephone          VARCHAR2 (32)  NOT NULL  ,
 	Email              VARCHAR2 (64) NOT NULL  ,
 	Adresse            VARCHAR2 (256) NOT NULL  ,
 	CONSTRAINT Adherent_PK PRIMARY KEY (ID)
@@ -46,7 +49,7 @@ CREATE TABLE Bibliothecaire(
 	Nom                VARCHAR2 (32) NOT NULL  ,
 	Prenom             VARCHAR2 (32) NOT NULL  ,
 	Date_de_naissance  DATE  NOT NULL  ,
-	Telephone          NUMBER(10,0)  NOT NULL  ,
+	Telephone          VARCHAR2 (32)  NOT NULL  ,
 	Email              VARCHAR2 (64) NOT NULL  ,
 	Adresse            VARCHAR2 (256) NOT NULL  ,
 	CONSTRAINT Bibliothecaire_PK PRIMARY KEY (ID)
@@ -69,7 +72,7 @@ CREATE TABLE Ouvrage(
 	Titre              VARCHAR2 (256) NOT NULL  ,
 	Annee_de_parution  NUMBER(4,0)   ,
 	Langue             VARCHAR2 (32) NOT NULL  ,
-	Format             VARCHAR2 (32) NOT NULL  ,
+	Format             VARCHAR2 (32) NOT NULL CONSTRAINT ouvrage_check_format CHECK (Format IN ('Broché','Relié','Numérique','Audio')) ,
 	Edition            NUMBER(10,0)  NOT NULL  ,
 	Description        CLOB   ,
 	Couverture         BLOB   ,
@@ -79,23 +82,24 @@ CREATE TABLE Ouvrage(
 	CONSTRAINT Ouvrage_PK PRIMARY KEY (ISBN)
 
 	,CONSTRAINT Ouvrage_Classification_FK FOREIGN KEY (Tag) REFERENCES Classification(Tag)
-	,CONSTRAINT Ouvrage_Éditeur0_FK FOREIGN KEY (Nom) REFERENCES Éditeur(Nom)
+	,CONSTRAINT Ouvrage_Éditeur0_FK FOREIGN KEY (Nom) REFERENCES Editeur(Nom)
 );
+
 
 ------------------------------------------------------------
 -- Table: Emprunt
 ------------------------------------------------------------
 CREATE TABLE Emprunt(
-	ID                 NUMBER(10,0)  NOT NULL  ,
+	ID_Adherent        NUMBER(10,0)  NOT NULL  ,
 	ID_Bibliothecaire  NUMBER(10,0)  NOT NULL  ,
 	ISBN               NUMBER(10,0)  NOT NULL  ,
 	Date_de_debut      DATE  NOT NULL  ,
 	Date_de_fin        DATE  NOT NULL  ,
 	Rendu              NUMBER (1) NOT NULL  ,
-	CONSTRAINT Emprunt_PK PRIMARY KEY (ID,ID_Bibliothecaire,ISBN, Date_de_debut),
+	CONSTRAINT Emprunt_PK PRIMARY KEY (ID_Adherent,ID_Bibliothecaire,ISBN, Date_de_debut),
 	CONSTRAINT CHK_BOOLEAN_Rendu CHECK (Rendu IN (0,1))
 
-	,CONSTRAINT Emprunt_Adherent_FK FOREIGN KEY (ID) REFERENCES Adherent(ID)
+	,CONSTRAINT Emprunt_Adherent_FK FOREIGN KEY (ID_Adherent) REFERENCES Adherent(ID)
 	,CONSTRAINT Emprunt_Bibliothecaire0_FK FOREIGN KEY (ID_Bibliothecaire) REFERENCES Bibliothecaire(ID)
 	,CONSTRAINT Emprunt_Ouvrage1_FK FOREIGN KEY (ISBN) REFERENCES Ouvrage(ISBN)
 );
@@ -105,13 +109,12 @@ CREATE TABLE Emprunt(
 ------------------------------------------------------------
 CREATE TABLE Realise(
 	ISBN               NUMBER(10,0)  NOT NULL  ,
-	Nom                VARCHAR2 (32) NOT NULL  ,
-	Prenom             VARCHAR2 (32) NOT NULL  ,
-	Date_de_naissance  DATE  NOT NULL  ,
-	CONSTRAINT Realise_PK PRIMARY KEY (ISBN,Nom,Prenom,Date_de_naissance)
+	ID_Auteur          NUMBER(10,0)  NOT NULL  ,
+	Role			   VARCHAR2 (32) NOT NULL  CONSTRAINT realise_check_role CHECK (Role IN ('ecrire','illustre','participe')) ,
+	CONSTRAINT Realise_PK PRIMARY KEY (ISBN,ID_Auteur)
 
 	,CONSTRAINT Realise_Ouvrage_FK FOREIGN KEY (ISBN) REFERENCES Ouvrage(ISBN)
-	,CONSTRAINT Realise_Auteur0_FK FOREIGN KEY (Nom,Prenom,Date_de_naissance) REFERENCES Auteur(Nom,Prenom,Date_de_naissance)
+	,CONSTRAINT Realise_Auteur0_FK FOREIGN KEY (ID_Auteur) REFERENCES Auteur(ID)
 );
 
 
@@ -120,6 +123,7 @@ CREATE TABLE Realise(
 
 CREATE SEQUENCE Seq_Adherent_ID START WITH 1 INCREMENT BY 1 NOCYCLE;
 CREATE SEQUENCE Seq_Bibliothecaire_ID START WITH 1 INCREMENT BY 1 NOCYCLE;
+CREATE SEQUENCE Seq_Autheur_ID START WITH 1 INCREMENT BY 1 NOCYCLE;
 
 
 CREATE OR REPLACE TRIGGER Adherent_ID
@@ -136,5 +140,13 @@ CREATE OR REPLACE TRIGGER Bibliothecaire_ID
 	WHEN (NEW.ID IS NULL) 
 	BEGIN
 		 select Seq_Bibliothecaire_ID.NEXTVAL INTO :NEW.ID from DUAL; 
+	END;
+	/
+CREATE OR REPLACE TRIGGER Auteur_ID
+	BEFORE INSERT ON Auteur 
+  FOR EACH ROW 
+	WHEN (NEW.ID IS NULL) 
+	BEGIN
+		 select Seq_Autheur_ID.NEXTVAL INTO :NEW.ID from DUAL; 
 	END;
 	/
